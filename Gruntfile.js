cd6e802155d73eabc4e073bdf2945ce53e93c7a5
cmanessis <cmpetrowii@gmail.com>
2015-06-09 14:11:08 +0100

    added grunt jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,15 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
+      jasmine:{
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
         globals: {
           jQuery: true
         }
       }
-    },
+    }
+  },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -17,7 +19,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint'], ['jasmine']);
 
 };
