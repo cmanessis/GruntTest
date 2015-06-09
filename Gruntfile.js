542c41538803e574326ce553980e3abb65dff16b
cmanessis <cmpetrowii@gmail.com>
2015-06-09 12:17:07 +0100

    Added grunt-contrib-watch-plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,22 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js']
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint']);
 
